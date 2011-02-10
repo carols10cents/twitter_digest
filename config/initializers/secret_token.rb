da85bf1a273ceff90ffdb2681db7015b2edc3f55
Carol Nichols <carol.nichols@gmail.com>
2011-02-10 14:42:40 -0500

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterDigest::Application.config.secret_token = '8b7f5888253675a7dd4b785a9c1a535188a2167ca5436941f631a52fa76f82a50c84b72c184de8673ca19dbc33642d278929f22b3229b65a253bffe2550e3c66'
