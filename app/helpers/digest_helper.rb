de6af62eb6dee451bc7b02f168bd18f8709b2c30
Carol Nichols <nichols@vivisimo.com>
2011-02-14 10:00:59 -0500

    1st level conversation detection

--- app/helpers/digest_helper.rb
+++ app/helpers/digest_helper.rb
@@ -1,15 +1,27 @@
 module DigestHelper
   def digest_tweets(tweets)
     digested = []
+    pulled_into_conversation = []
     tweets.each do |tweet|
-      conversation = []
       reply_to = tweet.in_reply_to_status_id_str
+
       if reply_to
-        conversation << tweets.select{|x| x.id.to_s.eql?(reply_to)} 
-        if !conversation.empty?
-          digested << ([tweet, conversation].flatten)
+        existing_conversation = digested.find{|c| c[:ids] && c[:ids].include?(reply_to)}
+        if existing_conversation
+          existing_conversation[:conversation] << tweet
+          existing_conversation[:conversation].sort!{|a, b| b.created_at <=> a.created_at}
+          existing_conversation[:ids] = existing_conversation[:conversation].collect{|c| c.id}
+
+        else
+          orig = tweets.find{|t| t.id == reply_to}
+          if orig
+            digested << {:conversation => [tweet, orig],
+                         :ids          => [tweet.id, orig.id]
+                        }
+            pulled_into_conversation << orig.id
+          end
         end
-      else
+      elsif !pulled_into_conversation.include?(tweet.id)
         digested << tweet
       end
     end
--- spec/helpers/digest_helper_spec.rb
+++ spec/helpers/digest_helper_spec.rb
@@ -1,20 +1,48 @@
 require 'spec_helper'
 
 describe DigestHelper do
-  describe "digest_tweets" do
+  describe "#digest_tweets" do
     before(:each) do
-      @tweet1 = double("tweet", :id => 1, :in_reply_to_status_id_str => nil)
-      @tweet2 = double("tweet", :id => 2, :in_reply_to_status_id_str => nil)
-      @tweet3 = double("tweet", :id => 3, :in_reply_to_status_id_str => 2)
+      #created_at = Sun Feb 13 18:22:56 +0000 2011
+      @tweet1 = double("tweet1",
+                       :id => 1,
+                       :in_reply_to_status_id_str => nil,
+                       :created_at => "Sun Feb 13 18:00:00 +0000 2011")
+      @tweet2 = double("tweet2",
+                       :id => 2,
+
+                       :in_reply_to_status_id_str => nil,
+                       :created_at => "Sun Feb 13 18:01:00 +0000 2011")
+      @tweet3 = double("tweet3",
+                       :id => 3,
+                       :in_reply_to_status_id_str => 2,
+                       :created_at => "Sun Feb 13 18:02:00 +0000 2011")
+      @tweet4 = double("tweet4",
+                       :id => 4,
+                       :in_reply_to_status_id_s       tr => 2,
+                       :created_at => "Sun Feb 13 18:03:00 +0000 2011")
     end
-    
-    it "should not modify tweets that aren't in conversations" do
+
+    it "should not group tweets that aren't in conversations" do
       helper.digest_tweets([@tweet2, @tweet1]).should eql([@tweet2, @tweet1])
     end
-    
+
     it "should group a tweet with what it was in reply to" do
-      helper.digest_tweets([@tweet3, @tweet2, @tweet1]).should 
-        eql([[@tweet3, @tweet2], @tweet1])
+      helper.digest_tweets([@tweet3, @tweet2, @tweet1]).should eql([
+        {:conversation => [@tweet3, @tweet2],
+         :ids          => [@tweet3.id, @tweet2.id]
+        },
+        @tweet1
+      ])
+    end
+
+    it "should group tweets that are in reply to the same thing and order by time" do
+      helper.digest_tweets([@tweet4, @tweet3, @tweet2, @tweet1]).should eql([
+        {:conversation => [@tweet4, @tweet3, @tweet2],
+         :ids          => [@tweet4.id, @tweet3.id, @tweet2.id]
+        },
+        @tweet1
+      ])
     end
   end
 end
