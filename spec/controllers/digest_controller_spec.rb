db35a581106b6b34b479bb5923c89bb85dcf4bf9
Carol Nichols <carol.nichols@gmail.com>
2011-02-27 17:16:48 -0500

    Updating specs

--- spec/controllers/digest_controller_spec.rb
+++ spec/controllers/digest_controller_spec.rb
@@ -17,37 +17,19 @@ describe DigestController do
 
     describe "GET 'index'" do
       it "asks twitter for the most recent 800 tweets" do
-        @test_client.should_receive(:home_timeline)
-                    .with(:count            => 200,
-                          :include_entities => true)
-                    .and_return([@test_tweet])
+        TwitterDigest::TwitterAPI.should_receive(:request_home_timeline)
+                                 .with(:client          => @test_client,
+                                       :last_tweet_seen => nil)
+                                 .and_return([@test_tweet])
 
-        @test_client.should_receive(:home_timeline)
-                    .with(:page             => 2,
-                          :count            => 200,
-                          :include_entities => true)
-                    .and_return([@test_tweet])
-
-        @test_client.should_receive(:home_timeline)
-                    .with(:page             => 3,
-                          :count            => 200,
-                          :include_entities => true)
-                    .and_return([@test_tweet])
-
-        @test_client.should_receive(:home_timeline)
-                    .with(:page             => 4,
-                          :count            => 200,
-                          :include_entities => true)
-                    .and_return([@test_tweet])
-
-        Tweet.stub(:digest).and_return([@test_tweet])
+        TwitterDigest::DigestionLogic.stub(:digest).and_return([@test_tweet])
         get :index
       end
 
       it "sets the user's last_tweet_seen" do
-        @test_client.stub(:home_timeline)
-                    .and_return([@test_tweet])
-        Tweet.stub(:digest).and_return([@test_tweet])
+        TwitterDigest::TwitterAPI.stub(:request_home_timeline)
+                                 .and_return([@test_tweet])
+        TwitterDigest::DigestionLogic.stub(:digest).and_return([@test_tweet])
         @test_user.should_receive(:update_attributes!)
                   .with(:last_tweet_seen => 9000)
         get :index
@@ -62,62 +44,44 @@ describe DigestController do
     end
 
     it "requests only tweets since last time" do
-      @test_client.should_receive(:home_timeline)
-                  .with(:since_id         => 8000,
-                        :count            => 200,
-                        :include_entities => true)
-                  .and_return([@test_tweet])
-
-      @test_client.should_receive(:home_timeline)
-                  .with(:page             => 2,
-                        :since_id         => 8000,
-                        :count            => 200,
-                        :include_entities => true)
-                  .and_return([@test_tweet])
-      @test_client.should_receive(:home_timeline)
-                  .with(:page             => 3,
-                        :since_id         => 8000,
-                        :count            => 200,
-                        :include_entities => true)
-                  .and_return([@test_tweet])
-      @test_client.should_receive(:home_timeline)
-                  .with(:page             => 4,
-                        :since_id         => 8000,
-                        :count            => 200,
-                        :include_entities => true)
-                  .and_return([@test_tweet])
-      Tweet.stub(:digest).and_return([@test_tweet])
+      TwitterDigest::TwitterAPI.should_receive(:request_home_timeline)
+                               .with(:client          => @test_client,
+                                     :last_tweet_seen => 8000)
+                               .and_return([@test_tweet])
+      TwitterDigest::DigestionLogic.stub(:digest).and_return([@test_tweet])
       get :index
     end
 
     context "with tweets since last time" do
       it "should update last_tweet_seen" do
-        @test_client.stub(:home_timeline).and_return([@test_tweet])
-        Tweet.stub(:digest).and_return([@test_tweet])
+        TwitterDigest::TwitterAPI.stub(:request_home_timeline)
+                                 .and_return([@test_tweet])
+        TwitterDigest::DigestionLogic.stub(:digest).and_return([@test_tweet])
         @test_user.should_receive(:update_attributes!)
                   .with(:last_tweet_seen => 9000)
         get :index
       end
 
       it "should have a count for @num_unabridged_tweets" do
-        @test_client.stub(:home_timeline).and_return([@test_tweet])
-        Tweet.stub(:digest).and_return([@test_tweet])
+        TwitterDigest::TwitterAPI.stub(:request_home_timeline)
+                                 .and_return([@test_tweet])
+        TwitterDigest::DigestionLogic.stub(:digest).and_return([@test_tweet])
         get :index
-        assigns[:num_unabridged_tweets].should eql(4)
+        assigns[:num_unabridged_tweets].should eql(1)
       end
     end
 
     context "with no tweets since last time" do
       it "should not update last_tweet_seen" do
-        @test_client.stub(:home_timeline).and_return([])
-        Tweet.stub(:digest).and_return([])
+        TwitterDigest::TwitterAPI.stub(:request_home_timeline).and_return([])
+        TwitterDigest::DigestionLogic.stub(:digest).and_return([])
         @test_user.should_not_receive(:update_attributes!)
         get :index
       end
 
       it "should have a 0 count for @num_unabridged_tweets" do
-        @test_client.stub(:home_timeline).and_return([])
-        Tweet.stub(:digest).and_return([])
+        TwitterDigest::TwitterAPI.stub(:request_home_timeline).and_return([])
+        TwitterDigest::DigestionLogic.stub(:digest).and_return([])
         get :index
         assigns[:num_unabridged_tweets].should eql(0)
       end
