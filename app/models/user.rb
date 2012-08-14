3111b79a61bac48daa136ee276c35cffd24d6262
Carol Nichols <carol.nichols@gmail.com>
2012-08-14 15:57:45 -0400

    Changing user_info from omniauth to just info

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ActiveRecord::Base
     create! do |user|
       user.provider = auth["provider"]
       user.uid      = auth["uid"]
-      user.name     = auth["user_info"]["name"]
-      user.nickname = auth["user_info"]["nickname"]
+      user.name     = auth["info"]["name"]
+      user.nickname = auth["info"]["nickname"]
       user.token    = auth["credentials"]["token"]
       user.secret   = auth["credentials"]["secret"]
     end
